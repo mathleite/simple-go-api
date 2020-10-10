33eb787b84ea4dfef79d9724c2b15dfa3468899f
Matheus Leite <matheus.leite.san@gmail.com>
2020-10-10 12:48:51 -0300

    Create Mux router package

---
+++ app/router/mux.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"app/controller"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Initialize Mux Router
+func Initialize() {
+	// Init Router
+	router := mux.NewRouter()
+
+	// Route Handlers/Endpoint
+	router.HandleFunc("/api/books", controller.GetBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", controller.GetBook).Methods("GET")
+	router.HandleFunc("/api/books", controller.CreateBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", controller.DeleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
